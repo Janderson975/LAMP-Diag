f1a5fe803c44ef6ec4475aec6d8504139057bbc4
Janderson975 <131876522+Janderson975@users.noreply.github.com>
2023-08-26 16:08:37 -0400

    Update StartScoring.sh

--- StartScoring.sh
+++ StartScoring.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-sleep 10
 #sudo apt install git
 #sudo apt install shc
 #sudo apt install gcc
 git clone -b cloning https://github.com/Janderson975/LAMP-Diag.git
+sleep 20
 chmod +x LAMP-Diag/scorebot.sh
 mv LAMP-Diag/scorebot.sh /var/.score/scorebot.sh
 mv LAMP-Diag/ScoreReport.html /home/cyber/Desktop/ScoreReport.html
-mv LAMP-Diag/README.md /home/cyber/Desktop/README.md
+mv LAMP-Diag/README.html /home/cyber/Desktop/README.html
 chown cyber:cyber /home/cyber/Desktop/ScoreReport.html
 chown cyber:cyber /home/cyber/Desktop/README.md
 shc -f /var/.score/scorebot.sh
